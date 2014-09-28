7105a76e789433e5dd87bbb4fcf888595057225f
Nitish Shukla <nitish16shukla87@hotmail.com>
2014-09-28 17:49:30 +0530

    Composite design pattern

---
+++ src/LeafNode.java
@@ -0,0 +1,22 @@
+
+public class LeafNode extends Node{
+
+    private String name;
+    private String salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
